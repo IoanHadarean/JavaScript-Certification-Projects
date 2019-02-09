f5f28365c2391f4acfd504fcfa10d275612e3ac1
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-02-09 18:31:25 +0000

    Refactored caesars cipher code

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -1,8 +1,8 @@
 function rot13(str) { // LBH QVQ VG!
     var newArr = [];
     var result = [];
-    var banana = str.split("");
-    console.log(banana);
+    var str_split = str.split("");
+    console.log(str_split);
     var arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     console.log(arr);
     var reversedArr = arr.slice().reverse();
@@ -10,39 +10,39 @@ function rot13(str) { // LBH QVQ VG!
 
     for (var i = 0; i < arr.length; i++) {
         if (i <= (arr.length / 2) - 1) {
-            var a = arr[i + 13];
-            var b = newArr.push(a);
+            var value = arr[i + 13];
+            newArr.push(value);
         }
     }
     console.log(arr.length);
     for (var j = 0; j < reversedArr.length; j++) {
         if (j <= (reversedArr.length / 2) - 1) {
-            var c = reversedArr[j + 13];
-            var d = newArr.push(c);
+            var reversedValue = reversedArr[j + 13];
+            newArr.push(reversedValue);
         }
     }
     console.log(newArr);
 
-    for (var k = 0; k < banana.length; k++) {
-        if (newArr.indexOf(banana[k]) > -1 && newArr.indexOf(banana[k]) <= 12) {
-            var e = newArr.indexOf(banana[k]);
-            console.log(e);
-            var r = result.push(arr[e]);
+    for (var k = 0; k < str_split.length; k++) {
+        if (newArr.indexOf(str_split[k]) > -1 && newArr.indexOf(str_split[k]) <= 12) {
+            var newArrIndex = newArr.indexOf(str_split[k]);
+            console.log(newArrIndex);
+            result.push(arr[newArrIndex]);
         }
-        else if (newArr.indexOf(banana[k]) > -1 && newArr.indexOf(banana[k]) > 12) {
-            var salad = arr.indexOf(banana[k]);
-            console.log(e);
-            var reg = result.push(newArr[salad]);
+        else if (newArr.indexOf(str_split[k]) > -1 && newArr.indexOf(str_split[k]) > 12) {
+            var arrIndex = arr.indexOf(str_split[k]);
+            console.log(newArrIndex);
+            result.push(newArr[arrIndex]);
         }
         else {
-            var g = result.push(banana[k]);
+            result.push(str_split[k]);
         }
     }
     console.log(result);
-    var result2 = result.toString();
-    var result3 = result2.replace(/,/g, "");
-    console.log(result3);
-    return result3;
+    var toString = result.toString();
+    var replaced = toString.replace(/,/g, "");
+    console.log(replaced);
+    return replaced;
 }
 
 // Change the inputs below to test
